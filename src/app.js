defbe456c6c97844fdfc5e43cd0fa3507cadd8c1
andresrokp <afrokap@gmail.com>
2023-12-19 12:45:57 -0500

    chore: added cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
+const cors = require('cors');
 
 const express = require('express');
 
 const app = express(); // ejecuta express y asigna el servidor a la variable app
 app.use(express.json()); // decirle a la app que va a parsear JSON
+app.use(cors())
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Servidor escuchando. Puerto ${process.env.PORT}`);
