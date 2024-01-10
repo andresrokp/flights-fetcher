a4c9f1d36684ea73f2ec9b93d50b118289a61e48
andresrokp <afrokap@gmail.com>
2024-01-10 12:06:57 -0500

    feat: deleting all previous data

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const fetchFromApi = require('./fetchFromApi.js');
+const deleteTelemetry = require('./deleteTelemetry.js');
+
+const { entityType,entityId,keys,deleteAllDataForKeys,startTs,endTs,rewriteLatestIfDeleted,authorizationToken } = process.env;
 
 const app = express(); // ejecuta express y asigna el servidor a la variable app
 app.use(express.json()); // decirle a la app que va a parsear JSON
@@ -12,7 +15,9 @@ app.listen(process.env.PORT, ()=>{
 })
 
 app.get('/',async (req,res)=>{
+    await deleteTelemetry(entityType, entityId, keys, true, 0, Date.now(), rewriteLatestIfDeleted, authorizationToken);
+    console.log('Telemetry deleted successfully');
     const data = await fetchFromApi();
-    console.log(data);
+    // console.log(data);
     res.json(data)
 })
